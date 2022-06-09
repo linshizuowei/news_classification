78623700386e0113a9be380f301eebe509d14079
linshizuowei <wyl190420@gmail.com>
2022-06-09 12:16:00 +0800

    update

---
+++ models/gbdt.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os
+import json
+import sys
+
+
+class GradientBoostingDecisionTree(object):
+    """
+    """
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
