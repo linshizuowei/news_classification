f24bcbd7c787fbc4eebba363e9318af51400f12b
linshizuowei <wyl190420@gmail.com>
2022-06-13 11:32:12 +0800

    update

--- models/gbdt.py
+++ models/gbdt.py
@@ -3,16 +3,24 @@ import os
 import json
 import sys
 
+from .decision_tree import DecisionTreeCartReg
+
 
 class GradientBoostingDecisionTree(object):
     """
     """
 
-    def __init__(self):
+    def __init__(self, config):
+        self.tree_cnt = config.get('trees', 5)
+        self.deepth = config.get('deepth', 3)
+        self.leaf_nodes = config.get('leaf_nodes', 8)
+        self.learning_rate = config.get('learning_rate', 0.1)
+
+    def fit(self, data, label):
+        # build first tree
         pass
 
-    def fit(self):
-        pass
+        # build other trees
 
     def predict(self):
         pass
