ed9179a4b41683393b5bfd5e78b3007292970a21
linshizuowei <wyl190420@gmail.com>
2022-07-22 10:40:25 +0800

    update

--- models/fastText.py
+++ models/fastText.py
@@ -4,6 +4,12 @@ import json
 
 
 class FastTextModel(object):
+    """
+    fastText的关键点在于Word embedding和subword embedding，subword embedding是在
+    单词内的n-gram，需要注意的是fastText并没有利用单词间的n-gram，这在英语中是适用
+    的，但在汉语中，句子都是字符组成的，就只存在字符间的n-gram，不存在字符内的n-gram了。
+
+    """
     def __init__(self):
         pass
 
